58399caae20e59e87fcf1cd4a6afb320eeb836b2
“Drmingler” <kayymedia@gmail.com>
2020-03-23 06:30:58 +0100

    feat: Add combine reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import {loadingBarReducer} from "react-redux-loading";
+
+import users from "./users";
+import authUser from "./authUser";
+import questions from "./questions";
+
+export default combineReducers({
+    users: users,
+    authUser: authUser,
+    questions : questions,
+    loadingBar : loadingBarReducer
+
+});
