e982fe7c0c9498d8384066ddc2c87030393fa278
“Drmingler” <kayymedia@gmail.com>
2020-03-23 06:55:44 +0100

    feat: Add store  to  index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {createStore} from "redux";
+import middleware from '../src/middleware'
+import reducer from '../src/reducers'
+import {Provider} from 'react-redux'
 
+const store = createStore(reducer,middleware);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
