69ec2d227b41a3c2d2e4862ef414476a92613736
“Drmingler” <kayymedia@gmail.com>
2020-03-25 06:13:28 +0100

    fix: LoginPage Component not setting Autheduser in the state

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,17 +1,40 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import {setAuthUser} from "../actions/authUser"
 class LoginPage extends React.Component {
+  state = {
+    choice: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const {choice} = this.state;
+    if(choice){
+      this.props.dispatch(setAuthUser(choice))
+      // Redirect to homepage
+    }
+
+  };
+
+  handleChange = e => {
+    const choice = e.target.value;
+    this.setState({ choice });
+  };
   render() {
-    const {users} = this.props;
+    const { users } = this.props;
     return (
       <div className={"form-container"}>
         <div>
-          <form>
-            <select>
-              {Object.keys(users).map((user)=>(
-                  <option key={users[user].id}>{`${users[user].name}`}</option>
-              ))
-              }
+          <h1>Login Page</h1>
+          <form onSubmit={this.handleSubmit}>
+            <select onChange={this.handleChange}>
+              <option >Select A User</option>
+              {Object.keys(users).map(user => (
+                <option
+                  key={users[user].id}
+                  value={users[user].id}
+                >{`${users[user].name}`}</option>
+              ))}
             </select>
             <div>
               <button type={"submit"}> Submit </button>
@@ -23,10 +46,10 @@ class LoginPage extends React.Component {
   }
 }
 
-function mapStateToProps({users}) {
+function mapStateToProps({ users }) {
   return {
     users
-  }
+  };
 }
 
 export default connect(mapStateToProps)(LoginPage);
