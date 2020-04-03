a1d6d734a10ebf5a6644757d36f3ccfcab8ae4af
“Drmingler” <kayymedia@gmail.com>
2020-04-03 05:31:43 +0200

    fix: Add Error file

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Render this functional component whenever a user goes to an unknown route
+function Error(props) {
+  return <h1>404 PAGE NOT FOUND</h1>;
+}
+
+export default Error
